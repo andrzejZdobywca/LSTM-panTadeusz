3a5169b69e962ee703218d9339f78860a5834621
Wiktor Daniec <wdaniec@walter.ii.uj.edu.pl>
2019-04-15 23:21:46 +0200

    Reformating code #2

--- network.py
+++ network.py
@@ -12,7 +12,7 @@ class Network(nn.Module):
         self.hidden_dim = hidden_dim
         self.embedding_dim = embedding_dim
 
-        self.embedding = nn.Embedding(len(tokens), embedding_dim) # tutaj embedding_dim = 16
+        self.embedding = nn.Embedding(len(tokens), embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.fc = nn.Linear(hidden_dim, len(tokens))
     
@@ -29,10 +29,15 @@ class Network(nn.Module):
 
 
 def train(net, data, epochs=5, seq_size=32, clip=5, lr=0.0003, val_freq=0.2):
+
+    # set optimizer and loss function
     optimizer = optim.Adam(net.parameters(), lr=lr)
     criterion = nn.CrossEntropyLoss()
+
+    # convert text to array of integers
     data = utils.convert_to_int(data)
 
+    # divide data into training and validation set
     val_idx = int((len(data)*(1-val_freq)))
     data, val_data = data[:val_idx], data[val_idx:]
 
@@ -40,22 +45,29 @@ def train(net, data, epochs=5, seq_size=32, clip=5, lr=0.0003, val_freq=0.2):
         net.cuda()
 
     for e in range(epochs):
+        # initialize hidden state
         hidden = net.init_hidden()
         running_loss = 0.0
+
         for i in range(len(data)-seq_size):
             x, y = torch.Tensor(data[i:(i+seq_size)]).long(), torch.Tensor(data[(i+1):(i+seq_size+1)]).long()
             if(TRAIN_ON_GPU):
                 x, y = x.cuda(), y.cuda()
+
             x = net.embedding(x)
             x = x.view(seq_size, 1, -1)
+
             hidden = tuple([each.data for each in hidden])
+
             net.zero_grad()
             out, hidden = net(x, hidden)
             loss = criterion(out, y)
             running_loss += loss
+
             loss.backward()
             nn.utils.clip_grad_norm_(net.parameters(), 5)
             optimizer.step()
+
             if(i % 250 == 0):
                 print("train_loss aftter {0} = {1:.7f}".format(i, running_loss.item()))
                 running_loss = 0.0
--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ def read_text(path):
 
 def convert_to_int(data):
     chars = set(data)
-    # wydaje mi sie, ze mozna to napisac po prostu jako
-    # chars_as_int = dict(enumerate(chars))
     chars_as_int = dict([(x, i) for i, x in enumerate(chars)])
     data_as_int = [chars_as_int[x] for x in data]
     return data_as_int
